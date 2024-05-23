31dae46a2480cc2db62ad861d2f99a3e3a82ca40
unknown <chenwayhao@gmail.com>
2024-05-24 00:12:02 +0800

    init

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
     # )
     # gpt_reply = response.choices[0]['message']['content'].replace('。','').strip()
 
-    response = openai.Completion.create(
+    response = openai.chat.completions.create(
         model = 'gpt-3.5-turbo',
         prompt = user_message,
         temperature = 0.5,
