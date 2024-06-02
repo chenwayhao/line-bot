cbcb0b5f443995a65a673c0baa484c83f0ecbd74
unknown <chenwayhao@gmail.com>
2024-06-03 01:03:14 +0800

    init

--- hotel.py
+++ hotel.py
@@ -42,7 +42,7 @@ def get_googledata(latitude, longitude, google_maps_apikey, activity):
         encoded_name = urllib.parse.quote(name)
         maps_url = f"https://www.google.com/maps/place/?q={encoded_name}"
         
-        photo_reference = result.get('photos', [{}])[0].get('photo_reference')
+        photo_reference = photos[0].get('photo_reference') if photos else None
         if photo_reference:
             thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference={photo_reference}&key={google_maps_apikey}"
         else:
