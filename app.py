cd8719d36f1fcb47aaef0291907b834df4f5deae
unknown <chenwayhao@gmail.com>
2024-06-04 19:14:03 +0800

    init

--- app.py
+++ app.py
@@ -232,48 +232,6 @@ def handle_location_message(event):
     template_message = get_googledata(latitude, longitude, google_maps_apikey, activity)
     line_bot_api.reply_message(event.reply_token, template_message)
 
-# def get_googledata(latitude, longitude, google_maps_apikey, activity):
-#     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=1000&type={activity}&language=zh-TW&key={google_maps_apikey}"
-#     response = requests.get(url)
-#     results = response.json().get('results', [])
-
-#     columns = []
-#     for result in results[:10]:  # Show up to 10 results
-#         name = result.get('name')
-#         if len(name) > 40:
-#             name = name[:37] + '...'
-#         address = result.get('vicinity')
-#         rating = result.get('rating', 'N/A')
-#         place_id = result.get('place_id')
-
-#         encoded_name = urllib.parse.quote(name)
-#         maps_url = f"https://www.google.com/maps/place/?q={encoded_name}"
-        
-#         photo_reference = result.get('photos', [{}])[0].get('photo_reference')
-#         if photo_reference:
-#             thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference={photo_reference}&key={google_maps_apikey}"
-#         else:
-#             thumbnail_image_url = "https://via.placeholder.com/800x400?text=No+Image"
-
-#         column = CarouselColumn(
-#             thumbnail_image_url=thumbnail_image_url,
-#             title=name,
-#             text=f"評分: {rating}\n地址：{address}",
-#             actions=[
-#                 {
-#                     "type": "uri",
-#                     "label": "View on Map",
-#                     "uri": maps_url
-#                 }
-#             ]
-#         )
-#         columns.append(column)
-    
-#     carousel_template = CarouselTemplate(columns=columns)
-#     template_message = TemplateSendMessage(alt_text=f'Nearby {activity}', template = carousel_template)
-
-#     return template_message
-
 def get_googledata(latitude, longitude, google_maps_apikey, activity):
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=1000&type={activity}&language=zh-TW&key={google_maps_apikey}"
     response = requests.get(url)
@@ -300,7 +258,7 @@ def get_googledata(latitude, longitude, google_maps_apikey, activity):
     for place in places_data:
         gpt_input += f"名稱: {place['name']}, 地址: {place['address']}, 評分: {place['rating']}\n"
     
-    gpt_reply = gpt4_message(gpt_input)
+    gpt_reply = gpt35_message(gpt_input)
 
     sorted_places_data = parse_gpt_reply(gpt_reply, places_data)
 
